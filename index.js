1504cb028543dd5f8cc91087757a226b1d956dba
Tom Nielsen <tanielsen@gmail.com>
2020-11-12 13:24:41 +0000

    image upload to saltcorn

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const {
   div,
   text,
   script,
-  domReady,textarea,
-  style
+  domReady,
+  textarea,
+  style,
 } = require("@saltcorn/markup/tags");
 
 const headers = [
   {
-    script: "https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"
+    script:
+      "https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js",
   },
   {
-    css: "https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css"
-  }
+    css:
+      "https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css",
+  },
 ];
 
 const Summernote = {
@@ -22,20 +25,46 @@ const Summernote = {
   run: (nm, v, attrs, cls) =>
     div(
       {
-        class: [cls]
+        class: [cls],
       },
       textarea(
         {
           name: text(nm),
           id: `input${text(nm)}`,
-          rows: 10
+          rows: 10,
         },
         v || ""
       ),
       script(
-        domReady(`$('#input${text(nm)}').summernote({height:150});`)
+        domReady(`$('#input${text(nm)}').summernote({
+          height:150,
+          callbacks: {
+            onImageUpload: function(files) {
+              data = new FormData();
+              data.append("file", files[0]);
+              data.append("min_role_read", 10);
+              $.ajax({
+                data: data,
+                type: "POST",
+                url: "/files/upload",
+                cache: false,
+                headers: {
+                  "CSRF-Token": _sc_globalCsrf,
+                },
+                contentType: false,
+                processData: false,
+                success: function(resp) {
+                  var baseurl = window.location.origin;
+                  var url=window.location.origin+resp.success.url
+                  var image = $('<img>').attr('src', url);
+                  $('#input${text(nm)}').summernote("insertNode", image[0]);
+                }
+              });
+            }
+          }
+        });`)
       )
-    )
+    ),
 };
 
 const dependencies = ["@saltcorn/html"];
@@ -44,5 +73,5 @@ module.exports = {
   sc_plugin_api_version: 1,
   fieldviews: { Summernote },
   headers,
-  dependencies
+  dependencies,
 };
